00049f9fc523e8a3454049a27ac783cdc4ac5144
Walid Khalfa <43506524+Walid-Khalfa@users.noreply.github.com>
2023-05-24 14:08:11 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,34 +1,34 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div className="home">
       <h2>Welcome to our online store!</h2>
       <p>
-        We have thousands of books, electronics, toys and more available for 
+        We have thousands of books, electronics, toys and more available for         
         purchase. Browse our aisles and find exactly what you're looking for.
-      </p>  
+      </p>   
       <p>
         Browse our curated collections:
       </p>
-      <ul>     
-        <li>
-          <a href="/store?category=books">Books</a>
-        </li>  
-        <li>
-          <a href="/store?category=electronics">Electronics</a>      
+      <ul>                 
+        <li>          
+          <Link to="/store?category=books">Books</Link>        
+        </li>               
+        <li>          
+          <Link to="/store?category=electronics">Electronics</Link>              
         </li>
-        <li>     
-           <a href="/store?category=toys">Toys</a>
+        <li>           
+          <Link to="/store?category=toys">Toys</Link>
         </li>
-        {/* Other categories links */}       
-      </ul>      
+        {/* Other categories links */}              
+      </ul>              
       <p>
-        Have fun shopping!    
+        Have fun shopping!               
       </p>
     </div>  
   );
 };
 
 export default Home;
-
